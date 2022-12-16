57d18ace9193c3bbf2523d9d88bd14bd9e94cd19
Kyle Moseby <1486937+kylemoseby@users.noreply.github.com>
2022-12-16 09:25:42 -0800

    Assessment Fixes

--- js/bluefire.js
+++ js/bluefire.js
@@ -1,24 +1,39 @@
-$(function(){
-	$('#menu ul > li > a').on('click', function(){
-		$('.menuopen').slideUp(250).removeClass('menuopen');	
-		$(this).parent().find('.submenu').slideDown(250).addClass('menuopen');	
-	});
+$(function() {
 
-	$(document).on('click', function(){
-		$('.menuopen').slideUp(250).removeClass('menuopen');	
-	});
-	
-	$(window).resize(function (){
-		$('.menuopen').hide().removeClass('menuopen');	
-		$('#menu').removeClass('mainmenuopen');	
-	});
-	
-	$('#mobileMenuBtn').on('click', function(){
-		if($('#menu').hasClass('mainmenuopen')){
-			$('#menu').slideUp(250).removeClass('mainmenuopen');
-		}else{		
-			$('#menu').slideDown(250).addClass('mainmenuopen');	
-		}		
-	});
-	
+  let openTab = $();
+  const menu = document.querySelector('#menu');
+  const slideSpeed = 250;
+
+  $('#menu ul > li > a').on('click', function() {
+    let clickedTab = $(this).parent().find('.submenu');
+    if (!clickedTab.is(openTab)) {
+      openTab.slideUp(slideSpeed);
+      clickedTab.slideDown(slideSpeed);
+      openTab = clickedTab;
+    } else {
+      clickedTab.slideUp(slideSpeed);
+    }
+  });
+
+  $(document).on('click', function(ev) {
+    if (!$.contains(menu, ev.target)) {
+      openTab.slideUp(slideSpeed);
+      openTab = $();
+    }
+  });
+
+  $(window).resize(function() {
+    openTab.slideUp(slideSpeed);
+    // Should be same as min-width breakpoint in bluefire.js
+    if(window.innerWidth < 1301){
+      $('#menu').slideUp(0);
+    } else {
+      $('#menu').slideDown(0);
+    }
+  });
+
+  $('#mobileMenuBtn').on('click', function() {
+    $('#menu').slideToggle(slideSpeed);
+  });
+
 });
